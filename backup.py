7d77d1625b6bc4e9a387366379976cbd23732f39
August Montalbano <august@fireflydigital.com>
2021-08-21 18:26:44 -0500

    adding start of checksum calc

--- backup.py
+++ backup.py
@@ -2,6 +2,7 @@ import os
 import sys
 import boto3
 import logging
+import hashlib
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('am-pc-backups')
@@ -30,12 +31,19 @@ def init():
     )]
     logging.debug(f'There are {len(existing_keys)} existing keys.')
 
-def upload_file(filepath):
+def upload_file(filepath, checksum):
+    
+
     try:
-        bucket.Object(f'{prefix}{filepath}').upload_file(filepath)
-    except e:
+        bucket.Object(f'{prefix}{filepath}')
+            .upload_file(filepath, 
+                        ExtraArgs={"Metadata": {"mykey": "myvalue"}})
+    except Exception as e:
         logging.error(e)
 
+
+def should_upload(filepath, checksum):
+    pass
 
 def process_dir(path):
     for current_path, dirs, files in os.walk(dir):
@@ -44,7 +52,14 @@ def process_dir(path):
             current_file_path = os.path.join(current_path, f)
             logging.debug('In file loop...')
             logging.debug(current_file_path)
-            upload_file(current_file_path)
+            checksum = ""
+            with open(current_file_path, "rb") as f:
+                file_bytes = f.read()
+                checksum = hashlib.sha256(file_bytes).hexdigest()
+
+            logging.debug(f"Checksum: {checksum}")
+            if should_upload(current_file_path, checksum):
+                upload_file(current_file_path)
         
         for d in dirs:
             logging.debug('In dir loop...')
